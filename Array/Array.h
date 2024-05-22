41814ccfe1cf2859e6f7428c84f32d50b5e6208a
Waqar Amin <vacaramin86@gmail.com>
2024-05-22 17:46:42 +0500

    Array templatized

--- Array/Array.h
+++ Array/Array.h
@@ -1,18 +1,26 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
 #include <iostream>
 #include <initializer_list>
 
 using namespace std;
 
+template <typename T>
 class Array{
     private:
         int length;
-        int* array;
+        T* array;
     public:
         Array(int length);
-        Array(initializer_list<int> init_list);
+        Array(initializer_list<T> init_list);
         Array(const Array& other) ;
         ~Array() ;
-        int& operator[](int index);
+        T& operator[](int index);
         int getLength() const ;
         void print() const;
 };
+
+#include "Array.tpp"
+
+#endif // ARRAY_H
